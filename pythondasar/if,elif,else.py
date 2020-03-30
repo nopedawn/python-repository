fe19844adae12f18e3e9c11e86d690b456c42c89
nopedawn <62634724+nopedawn@users.noreply.github.com>
2020-03-30 17:23:08 +0700

    if, elif, else

---
+++ pythondasar/if,elif,else.py
@@ -0,0 +1,15 @@
+# variable
+nilai = 80
+kkm = 75
+
+# if
+if nilai > kkm:
+	print("nilai anda", nilai, "selamat anda lulus")
+
+# elif
+elif nilai == kkm:
+	print("nilai anda", nilai, "nilai anda cukup bagus")
+
+# else
+else:
+	print("nilai anda", nilai, "maaf anda tidak lulus")
