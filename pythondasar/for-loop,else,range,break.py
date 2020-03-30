a112440895717020da985d96e586c370a9e358c5
nopedawn <62634724+nopedawn@users.noreply.github.com>
2020-03-30 20:22:47 +0700

    for-loop, else, range, break

---
+++ pythondasar/for-loop,else,range,break.py
@@ -0,0 +1,13 @@
+# variable
+angka = 8
+
+
+# for-loop, else, range, break
+for urutanAngka in range(0,10):
+	print(urutanAngka)
+
+	if urutanAngka is angka:
+		print('angka', angka, 'ada lur')
+		break
+	else:
+		print('angka', angka, 'tidak ada lur')
