5bcfa2eed782428d81436aab6958e1653a794282
nopedawn <62634724+nopedawn@users.noreply.github.com>
2020-03-30 14:23:44 +0700

    Update pythondasar.py

--- pythondasar.py
+++ pythondasar.py
@@ -1,1 +1,14 @@
+# print()
 print("halo dunia")
+
+
+# contoh lain
+a = 2
+b = 4
+print(a+b)
+
+# contoh lainnya lagi
+angka = 1000
+nambahAngka = 4000
+hasil = angka + nambahAngka
+print('angka', angka, 'ditambah', nambahAngka, 'hasilnya:', hasil)
