ec86b06c6a3dccf1ec047d6fe638c521276455de
nopedawn <62634724+nopedawn@users.noreply.github.com>
2020-03-30 21:02:54 +0700

    def-function

---
+++ pythondasar/function.py
@@ -0,0 +1,11 @@
+# membuat function / fungsi
+def beras(liter):
+	hargaBeras = 9000
+	hargaTotal = liter*hargaBeras
+	print('harga beras', liter, 'liter adalah:', hargaTotal)
+
+
+# hasil penghitungan harga dilakukan di bawah ini
+beras(2)
+beras(0.5)
+beras(10)
