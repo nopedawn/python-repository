b13af567756360c24e6db5aa1b1a9d5f7107eed6
nopedawn <62634724+nopedawn@users.noreply.github.com>
2020-03-30 14:29:31 +0700

    list, tuple, set, dictionary

--- macam-tipe-data.py
+++ macam-tipe-data.py
@@ -10,7 +10,6 @@ tipedata3 = {'piton', 400, False, 2.5}
 # Dictionary, menggunakan {key:value}
 tipedata4 = {"string":'piton', "integer":400, "boolean":False, "float":2.5}
 
-
 # mengeprint variable  diatas menggunakan print()
 print(tipedata1)
 print(tipedata2)
