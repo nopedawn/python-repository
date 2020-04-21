afb2a48b0ed77e6ceef7ad279da1c5ccd3362868
nopedawn <62634724+nopedawn@users.noreply.github.com>
2020-04-21 14:44:27 +0700

    text to speech using pyttsx3 library

---
+++ python/text2speech-offline.py
@@ -0,0 +1,13 @@
+# pip install pyttsx3 / pyttsx3 library
+import pyttsx3
+
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate', 130)
+engine.setProperty('volume', 100)
+engine.say("hello world")
+
+engine.runAndWait()
